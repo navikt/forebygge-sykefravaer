4b0a5a035e2ff7c9a319de5d6895685105c1da21
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-06-08 18:48:41 +0200

    oppdatert og lagt til alle avsnitt til appen, og parset de ut til klienten

--- src/komponenter/Redirect.tsx
+++ src/komponenter/Redirect.tsx
@@ -2,10 +2,10 @@ import React, { FunctionComponent } from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 
 const Redirect: FunctionComponent<RouteComponentProps> = (props) => {
-    if (window.location.pathname === "/") {
-        props.history.push("/forebygge-sykefravaer");
-    }
-    return <>{props.children}</>;
+  if (window.location.pathname === "/") {
+    props.history.push("/forebygge-sykefravaer");
+  }
+  return <>{props.children}</>;
 };
 
 export default withRouter(Redirect);
--- src/komponenter/dokument/Dokument.tsx
+++ src/komponenter/dokument/Dokument.tsx
@@ -3,24 +3,46 @@ import BEMHelper from "../../utils/bem";
 import "./dokument.less";
 import { Innholdstittel } from "nav-frontend-typografi";
 import { DocumentTypes } from "../../sanity-blocks/sanityTypes";
+import BlockContent from "@sanity/block-content-to-react";
+import { serializers } from "../../sanity-blocks/serializer";
+import "./dokument.less";
 
 type DocType = DocumentTypes | null;
 
 interface Props {
   innhold: DocType;
+  image?: React.ReactNode;
   children: React.ReactNode;
 }
 
+const cls = BEMHelper("dokument");
+
 const Dokument = (props: Props) => {
-  const cls = BEMHelper("dokument");
   return props.innhold ? (
     <div className={cls.className}>
       <div className={cls.element("wrapper")}>
-        <Innholdstittel>{props.innhold.title}</Innholdstittel>
+        {documentSetHeader(props.innhold)}
         {props.children}
       </div>
     </div>
   ) : null;
 };
+
+const documentSetHeader = (innhold: DocumentTypes) =>
+  innhold && innhold.mainImage
+    ? headerWithImage(innhold)
+    : header(innhold.title);
+
+const headerWithImage = (innhold: DocumentTypes) => {
+  return (
+    <div className={cls.element("header")}>
+      <BlockContent blocks={innhold.mainImage} serializers={serializers} />
+      <Innholdstittel className={cls.element("header-txt")}>
+        {innhold.title}
+      </Innholdstittel>
+    </div>
+  );
+};
+const header = (txt: string) => <Innholdstittel>{txt}</Innholdstittel>;
 
 export default Dokument;
