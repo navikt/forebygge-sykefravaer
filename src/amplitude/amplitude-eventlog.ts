4bdf7181163f264bc5a890964fb83f7277b70c55
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-06-16 15:53:15 +0200

    lagt til amplitude paa siden

---
+++ src/amplitude/amplitude-eventlog.ts
@@ -0,0 +1,21 @@
+import amplitude from "./amplitudeInstance";
+
+const miljo = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+const appkey = "#forebygge-sykefravaer-".concat(miljo);
+
+const hrefKey = (href: Location) => href.toString();
+
+export const skrivTilMalingBesokerSide = () => {
+  amplitude.logEvent(appkey.concat("-brukerbesokersiden"));
+};
+
+export const logLenkeTrykk = (
+  event: React.MouseEvent<HTMLElement | MouseEvent>,
+  href: Location | undefined
+) => {
+  event.preventDefault();
+  if (!!href) {
+    amplitude.logEvent(appkey.concat("-lenketrykk=").concat(hrefKey(href)));
+    window.location = href;
+  }
+};
