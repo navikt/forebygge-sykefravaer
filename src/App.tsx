c7e568c77f3978d13ac235df03230d0f63a86abb
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-09-28 14:24:38 +0200

    Sett grå som tekstfarge; slett ubrukte css-filer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Redirect from "./komponenter/Redirect";
 import ForebyggeSykefravaer from "./komponenter/ForebyggeSykefravaer";
+import './App.less';
 
 function App() {
   return (
