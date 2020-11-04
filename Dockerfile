a03e4a085e0a3d8787c80ff8b8f54b4a7d09683d
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-11-04 12:57:38 +0100

    Fjerne uthenting av sanity innhold på backend

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ RUN npm install -g node-cache@4.2.0
 RUN npm install -g jsdom@16.4.0
 RUN npm install -g request@2.88.2
 RUN npm install -g fs-extra@8.1.0
-RUN npm install -g @sanity/client@1.149.7
 RUN npm install -g console-stamp@0.2.9
 RUN npm install -g dotenv@8.2.0
 RUN npm install -g http-proxy-middleware@^1.0.6
--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const fs = require("fs-extra");
 const request = require("request");
 const jsdom = require("jsdom");
 const NodeCache = require("node-cache");
-const sanityClient = require("@sanity/client");
 const { getVimeoApiProxy } = require("./vimeo-api-proxy");
 const { getVimeoBilderProxy } = require("./vimeo-bilder-proxy");
 
@@ -23,25 +22,12 @@ server.use(helmet());
 const BASE_URL = "/forebygge-sykefravaer";
 const { JSDOM } = jsdom;
 const prop = "innerHTML";
-const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID.trim(),
-  dataset: process.env.SANITY_DATASET.trim(),
-  token: process.env.SANITY_TOKEN.trim(),
-  useCdn: false,
-});
 
 // Cache init
 const mainCacheKey = "sykefravaer-withMenu";
 const backupCacheKey = "sykefravaer-withMenuBackup";
-const mainCacheInnholdKey = "sykefravaer-innhold";
-const backupCacheInnholdKey = "sykefravaer-innholdBackup";
 const mainCacheMeny = new NodeCache({ stdTTL: 900, checkperiod: 90 });
 const backupCacheMeny = new NodeCache({ stdTTL: 0, checkperiod: 0 });
-const mainCacheInnhold = new NodeCache({
-  stdTTL: parseInt(process.env.SANITY_CACHE_TTL),
-  checkperiod: parseInt(process.env.SANITY_CACHE_CHECK),
-});
-const backupCacheInnhold = new NodeCache({ stdTTL: 0, checkperiod: 0 });
 
 //server response to pipeline
 server.get("/forebygge-sykefravaer/internal/isAlive", (req, res) =>
@@ -50,15 +36,6 @@ server.get("/forebygge-sykefravaer/internal/isAlive", (req, res) =>
 server.get("/forebygge-sykefravaer/internal/isReady", (req, res) =>
   res.sendStatus(200)
 );
-
-const sanityQueryTypes = () => [
-  "vi-hjelper-dere-med",
-  "digitale-tjenester",
-  "webinar-og-kurs",
-  "oppfolging-fra-nav-arbeidslivssenter",
-  "helseIArbeid",
-  "ia-avtalen",
-];
 
 const htmlinsert = () => [
   { inject: "styles", from: "styles" },
@@ -88,73 +65,7 @@ const url = () => devUrl;
 //  "https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&level=Level4&language=nb&breadcrumbs=" +
 //    JSON.stringify(brødsmulesti);
 
-const querystart = (len) => len === 0;
-
-const querySanity = () => {
-  let querystring = "";
-  sanityQueryTypes().forEach((elem, index) => {
-    querystring = querystring.concat(
-      querystart(index) ? `*[(_type == '${elem}'` : ` || _type == '${elem}'`
-    );
-  });
-  return querystring.concat(
-    ') && !(_id in path("drafts.**"))] | order(priority)'
-  );
-};
-
-const setHeaders = (responsheader) => {
-  responsheader.setHeader("Access-Control-Allow-Origin", "*");
-  responsheader.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  responsheader.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  responsheader.setHeader("Access-Control-Allow-Credentials", true);
-};
-
 const BUILD_PATH = path.join(__dirname, "../build");
-
-const sendDataObj = (json) => {
-  return {
-    data: json,
-    env: [process.env.SANITY_PROJECT_ID, process.env.SANITY_DATASET],
-  };
-};
-
-const checkbackupCacheInnhold = (res, fetchError) => {
-  const cacheBackupInnhold = backupCacheInnhold.get(backupCacheInnholdKey);
-  if (cacheBackupInnhold) {
-    mainCacheInnhold.set(mainCacheInnholdKey, cacheBackupInnhold);
-    res.send(sendDataObj(cacheBackupInnhold));
-  } else {
-    console.log("fetchError", fetchError);
-    res.send(fetchError);
-  }
-};
-
-const fetchInnhold = (res) => {
-  console.log("[DEBUG] fetchInnhold");
-  const query = querySanity();
-  client
-    .fetch(query)
-    .then((result) => {
-      mainCacheInnhold.set(mainCacheInnholdKey, result);
-      backupCacheInnhold.set(backupCacheInnholdKey, result);
-      res.send(sendDataObj(result));
-    })
-    .catch((error) => {
-      checkbackupCacheInnhold(res, error);
-    });
-};
-
-server.get(`${BASE_URL}/innhold/`, (req, res) => {
-  setHeaders(res);
-  const cacheInnhold = mainCacheInnhold.get(mainCacheInnholdKey);
-  cacheInnhold ? res.send(sendDataObj(cacheInnhold)) : fetchInnhold(res);
-});
 
 const injectMenuIntoHtml = (menu) => {
   fs.readFile(BUILD_PATH + "/index.html", "utf8", function (err, html) {
