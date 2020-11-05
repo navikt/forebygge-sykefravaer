3544981fa6779b28fe1264ce6bdefe7f55653101
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-11-05 09:15:28 +0100

    Bruk prod/dev dekoratør ift cluster applikasjon kjører på + prettier

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+require("console-stamp")(console, "[HH:MM:ss.l]");
+
 // env
 const VAULT_PATH = "/var/run/secrets/nais.io/vault/environment.env";
-require("console-stamp")(console, "[HH:MM:ss.l]");
 require("dotenv").config({
   path: process.env.NODE_ENV === "production" ? VAULT_PATH : ".env",
 });
