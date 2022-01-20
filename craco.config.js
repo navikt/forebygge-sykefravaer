95fbc1f7bc02ed32c5dfbb9fcc46f88ceb2a3b1b
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-01-20 08:34:38 +0100

    Oppgrader raect-scripts til versjon 5 + eslint til versjon 8

--- craco.config.js
+++ craco.config.js
@@ -3,36 +3,34 @@ const decoratorhtmlwebpackplugin = require("./plugins/decoratorhtmlwebpackplugin
 const path = require("path");
 
 module.exports = {
-  devServer: {
-    before: (app) => {
-      app.get("/forebygge-sykefravaer/api/thumbnail*", (req, res) =>
-        res.sendFile("videoThumbnail.jpg", {
-          root: path.join(__dirname, "./src/mocking/"),
-        })
-      );
+    devServer: {
+        before: (app) => {
+            app.get("/forebygge-sykefravaer/api/thumbnail*", (req, res) =>
+                res.sendFile("videoThumbnail.jpg", {
+                    root: path.join(__dirname, "./src/mocking/"),
+                })
+            );
+        },
     },
-  },
-  eslint: {
-    enable: true,
-    mode: "extends",
-    configure: {
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        sourceType: "module",
-      },
-      plugins: ["@typescript-eslint"],
-      extends: ["plugin:jsx-a11y/recommended"],
-      rules: {
-        "no-use-before-define": "off",
-        // Kan slås på når react-scripts oppgraderer sin avhengighet til eslint https://github.com/typescript-eslint/typescript-eslint/issues/2540
-        "@typescript-eslint/no-use-before-define": ["off"],
-      },
+    eslint: {
+        enable: true,
+        mode: "extends",
+        configure: {
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                sourceType: "module",
+            },
+            rules: {
+                "no-use-before-define": "off",
+                // Kan slås på når react-scripts oppgraderer sin avhengighet til eslint https://github.com/typescript-eslint/typescript-eslint/issues/2540
+                "@typescript-eslint/no-use-before-define": ["off"],
+            },
+        },
     },
-  },
-  plugins: [
-    { plugin: CracoLessPlugin },
-    {
-      plugin: decoratorhtmlwebpackplugin(process.env.ENABLE_EXTERNAL_MENU),
-    },
-  ],
+    plugins: [
+        {plugin: CracoLessPlugin},
+        {
+            plugin: decoratorhtmlwebpackplugin(process.env.ENABLE_EXTERNAL_MENU),
+        },
+    ],
 };
