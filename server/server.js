d268288020c82155811a2a1f39e2d49d4121e113
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-03-14 11:11:05 +0100

    Innføre csp (#141)
    * Legge til Constent-Security-Policy headers på req/res
    
    * Deploy til dev

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const { BASE_PATH } = require("./konstanter");
 const getDecorator = require("./decorator");
 const mustacheExpress = require("mustache-express");
 const express = require("express");
+const getCspValue = require('./csp');
 const app = express();
 const path = require("path");
 const buildPath = path.join(__dirname, "../build");
@@ -24,6 +25,24 @@ app.set("views", buildPath);
 
 const startServer = (html) => {
   app.use(BASE_PATH + "/", express.static(buildPath, { index: false }));
+  app.use((req, res, next) => {
+    res.header('X-Frame-Options', 'SAMEORIGIN');
+    res.header('X-Xss-Protection', '1; mode=block');
+    res.header('X-Content-Type-Options', 'nosniff');
+    res.header('Referrer-Policy', 'no-referrer');
+    res.header('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+    res.header('Content-Security-Policy', getCspValue());
+    if (process.env.NODE_ENV === 'development') {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
+      res.header('Access-Control-Allow-Methods', 'GET, POST');
+    }
+    next();
+  });
+
 
   app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
   app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
