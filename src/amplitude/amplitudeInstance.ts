cf94e848dfe0c5a81b58b0392286ed601f663727
runarskagestad <runar.skagestad@nav.no>
2022-05-31 13:23:26 +0200

    Navngav bucket-IDene til amplitude, slik at det er tydeligere hvilket prosjekt de tilhører

--- src/amplitude/amplitudeInstance.ts
+++ src/amplitude/amplitudeInstance.ts
@@ -1,13 +1,19 @@
 import amplitude from 'amplitude-js';
 
-const getApiKey = () => {
+const enum AmplitudeBucket {
+    ARBEIDSGIVER_PROD = 'a8243d37808422b4c768d31c88a22ef4',
+    ARBEIDSGIVER_DEV = '6ed1f00aabc6ced4fd6fcb7fcdc01b30',
+}
+
+const getBucketId = () => {
     return window.location.hostname === 'arbeidsgiver.nav.no'
-        ? 'a8243d37808422b4c768d31c88a22ef4'
-        : '6ed1f00aabc6ced4fd6fcb7fcdc01b30';
+        ? AmplitudeBucket.ARBEIDSGIVER_PROD
+        : AmplitudeBucket.ARBEIDSGIVER_DEV;
 };
 
 const amplitudeInstance = amplitude.getInstance();
-amplitudeInstance.init(getApiKey(), '', {
+
+amplitudeInstance.init(getBucketId(), '', {
     apiEndpoint: 'amplitude.nav.no/collect',
     saveEvents: false,
     includeUtm: true,
